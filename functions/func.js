f80199377fa38fc0b3c40b4f30f3f93756f23fdf
Владимир Яценко < >
2016-05-17 01:18:08 +0300

    no message

---
+++ functions/func.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Владимир on 17.05.2016.
+ */
+module.exports = function(arg1, callback){
+    /* тело твой функции, где после выполения всех действий ты должен будешь написать след строку */
+
+    callback({error:true,result:false});
+};
